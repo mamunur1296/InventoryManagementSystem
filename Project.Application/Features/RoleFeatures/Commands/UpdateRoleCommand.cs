bf619f855ec8936408da97f56b275d45c31b8407
Mamunur Rushid <mamunurrushid60@gmail.com>
2024-06-03 12:04:35 +0600

    some changes

--- Project.Application/Features/RoleFeatures/Commands/UpdateRoleCommand.cs
+++ Project.Application/Features/RoleFeatures/Commands/UpdateRoleCommand.cs
@@ -1,10 +1,5 @@
 ﻿using MediatR;
 using Project.Application.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Project.Application.Features.RoleFeatures.Commands
 {
